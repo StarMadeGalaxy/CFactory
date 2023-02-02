44caa39feed3d7d2b3e9cc841cf534034f1a8ffe
Vénci <42641919+StarMadeGalaxy@users.noreply.github.com>
2023-02-03 02:27:48 +0300

    Update cfactory.py
    working on linux

--- cfactory.py
+++ cfactory.py
@@ -4,7 +4,7 @@
 # |
 # +--+setup
 #    |
-#    +--+build.py
+#    +--+build.py  
 
 import subprocess
 import fnmatch
@@ -15,7 +15,10 @@ import json
 
 
 class CFactory:
-    def __init__(self, compiler="clang++", build_dir_name="build"):
+    def __init__(
+        self, compiler: str = "clang++", 
+        build_dir_name: str = "build", compiler_opts=list()):
+        
         self.project_root_path = os.path.dirname(os.getcwd())
         self.project_name = CFactory.get_last_name_path(self.project_root_path)
 
@@ -27,78 +30,91 @@ class CFactory:
 
         self.current_platform = platform.system()
 
+        self.static_libs = list()
+        self.dynamic_libs = list()
+
         self.compiler = compiler
         self.build_command: list = [compiler, "-o"]
-        self.compiler_opts = list()
+
+        CFactory.insert_after(self.build_command, self.compiler, compiler_opts)
 
     def build(
-            self, to_build: bool = True, delete_intermediate: bool = False,
+            self, to_build: bool = True, delete_intermediate: bool = False, 
             print_command: bool = False, update_cache: bool = False, extension: str = "cpp",
-            compile_without_linking_only: bool = False):
+            compile_without_linking_only: bool = False, remove_cache: bool = False):
 
         if update_cache:
             self.__update_cache(extension=extension)
 
         all_files_hash = dict(CFactory.get_files_dict_sha256(self.project_root_path, extension))
-        loaded_hash = CFactory.load_files_cache(self.json_cache_file_path)
-        files_to_compile = list(CFactory.get_changed_files(all_files_hash, loaded_hash))
+        loaded_hash = dict(CFactory.load_files_cache(self.json_cache_file_path))
+        changed_files = list(CFactory.get_changed_files(all_files_hash, loaded_hash))
 
-        cpp_sources = CFactory.find_files(self.project_root_path, extension)
-        static_libs = ["-static"]
-        dynamic_libs = list()
-        executable_name = self.project_name
-        # static_libs += CFactory.find_files(self.project_root_path, "so")
-
+        executable_name = self.project_name.lower()
+        
         if self.current_platform == "Windows":
-            dynamic_libs = CFactory.find_files(self.project_root_path, "dll")
-            static_libs += CFactory.find_files(self.project_root_path, "lib")
+            self.dynamic_libs += list(CFactory.find_files(self.project_root_path, "dll"))
+            self.static_libs += list(CFactory.find_files(self.project_root_path, "lib"))
             executable_name += ".exe"
-        elif self.current_platform == "Linux":
-            dynamic_libs = CFactory.find_files(self.project_root_path, "so")
+        elif self.current_platform == "Linux": 
+            self.dynamic_libs += list(CFactory.find_files(self.project_root_path, "so"))
+            self.static_libs += list(CFactory.find_files(self.project_root_path, "a"))
+            libs_directories = set(CFactory.find_files_dir(self.project_root_path, "so"))
+            self.__dynamic_linker_search_libs_path(libs_directories)
             executable_name += ".bin"
 
         CFactory.insert_after(self.build_command, "-o", [executable_name])
-        CFactory.insert_after(self.build_command, self.compiler, cpp_sources)
-        # CFactory.insert_after(self.build_command, executable_name, static_libs)
+        clang_libs_name = ["-l:" + CFactory.get_last_name_path(lib) for lib in self.dynamic_libs]
+        CFactory.insert_after(self.build_command, executable_name, clang_libs_name)
 
-        if to_build:
-            self.__goto_build_dir()
-            self.__compile_without_linking(files_to_compile)
-            object_files = CFactory.find_files(os.getcwd(), "o")
-            self.build_command = [opt for opt in self.build_command if extension not in opt]
+        self.__goto_build_dir()
+        self.__compile_without_linking(changed_files, print_command=print_command)
+
+        print("All files hash:", all_files_hash)
+        print("Loaded hash:", loaded_hash)
+        print("Changed files: ", changed_files)
+        
+        if to_build and not compile_without_linking_only:
+            object_files = list(CFactory.find_files(self.build_dir_path, "o"))
+            print("Object files:", object_files)
             CFactory.insert_after(self.build_command, self.compiler, object_files)
+            subprocess.call(self.build_command)
+            
+        if delete_intermediate:
+            for file in object_files:
+                os.remove(file)
 
-            if not compile_without_linking_only:
-                subprocess.call(self.build_command)
+        if remove_cache:
+            self.__remove_cache()
 
-            if delete_intermediate:
-                for file in object_files:
-                    os.remove(file)
         if print_command:
             self.__print_build_command()
 
     def __print_build_command(self):
-        print("Build command:", self.build_command)
+        print("Build command:", *self.build_command)
 
     def __compile_without_linking(
-            self, files_to_compile: list, to_build: bool = True, print_command: bool = False):
+        self, changed_files: list, to_build: bool = True, print_command: bool = False):
         clang_without_linking_flag = "-c"
         command = [self.compiler, clang_without_linking_flag]
-        CFactory.insert_after(command, clang_without_linking_flag, list(files_to_compile))
+        CFactory.insert_after(command, clang_without_linking_flag, changed_files)
 
         if print_command:
-            print("Compile without linking command:", files_to_compile)
+            print("Compile without linking command:", *command)
 
-        if files_to_compile:
+        if changed_files and to_build:
             subprocess.call(command)
 
     def __create_build_directory(self):
-        build_dir_path = CFactory.find_build_dir(self.project_root_path,
-                                                 self.build_dir_name)
-        self.build_dir_path = build_dir_path
+        build_dir_path = CFactory.find_build_dir(self.project_root_path, self.build_dir_name)
 
         if not build_dir_path:
-            os.mkdir(build_dir_path)
+            os.mkdir(self.build_dir_path)
+
+    def __dynamic_linker_search_libs_path(self, pathes: set):
+        dynamic_linker_rpath_list = ["-Wl", *["-rpath=" + path for path in pathes]]
+        linker_comma_sep_args = ','.join(dynamic_linker_rpath_list)
+        self.build_command += [linker_comma_sep_args]
 
     def __open_build_directory(self):
         os.chdir(self.build_dir_path)
@@ -112,15 +128,17 @@ class CFactory:
             os.remove(self.json_cache_file_path)
 
     def __update_cache(self, extension: str):
-        all_files_hash = dict(CFactory.get_files_dict_sha256(self.project_root_path, extension))
-        CFactory.dump_files_cache(self.json_cache_file_path, all_files_hash)
+        all_files_hash = CFactory.get_files_dict_sha256(self.project_root_path, extension)
+        CFactory.dump_files_cache(self.json_cache_file_path, dict(all_files_hash))
 
     @staticmethod
-    def get_changed_files(sources: dict, cache_dict: dict):
-        for file in sources:
-            if file not in cache_dict:
-                yield file
-            elif sources[file] != cache_dict[file]:
+    def get_changed_files(sources: dict, cache_dict: dict) -> list:
+        if cache_dict:
+            for file in sources:
+                if (file not in cache_dict) or (sources[file] != cache_dict[file]):
+                    yield file
+        else:
+            for file in sources:
                 yield file
 
     @staticmethod
@@ -131,7 +149,7 @@ class CFactory:
     def find_build_dir(root: str, build_dir_name: str) -> str:
         if build_dir_name in os.listdir(root):
             return os.path.join(root, build_dir_name)
-
+    
     @staticmethod
     def load_files_cache(cache_file_path: str) -> dict:
         if os.path.exists(cache_file_path):
@@ -145,10 +163,10 @@ class CFactory:
             json.dump(files_hashes, cache_file)
 
     @staticmethod
-    def get_files_dict_sha256(root: str, extension: str) -> tuple:
+    def get_files_dict_sha256(root: str, extension: str):
         for file in CFactory.find_files(root, extension):
             yield file, CFactory.get_file_sha256(file)
-
+        
     @staticmethod
     def get_file_sha256(file_path: str) -> str:
         hash_func = hashlib.sha256()
@@ -157,11 +175,19 @@ class CFactory:
                 data_chunk = read_file.read(hash_func.block_size)
                 hash_func.update(data_chunk)
                 if not data_chunk:
-                    break
+                    break        
         return hash_func.hexdigest()
 
+    # todo: several extensions check
     @staticmethod
-    def find_files(root: str, extension: str) -> list:
+    def find_files_dir(root: str, extension: str):
+        for cur_dir, sub_dir, files in os.walk(root):
+            for file in files:
+                if fnmatch.fnmatch(file, f"*.{extension}"):
+                    yield cur_dir
+
+    @staticmethod
+    def find_files(root: str, extension: str):
         for cur_dir, sub_dir, files in os.walk(root):
             for file in files:
                 if fnmatch.fnmatch(file, f"*.{extension}"):
@@ -169,14 +195,6 @@ class CFactory:
 
     @staticmethod
     def insert_after(dest: list, element_after: str, sources: list):
-        for source in sources:
+        for source in reversed(sources):
             dest.insert(dest.index(element_after) + 1, source)
-
-
-def main():
-    builder = CFactory()
-    builder.build(to_build=True, print_command=True)
-
-
-if __name__ == "__main__":
-    main()
+    
